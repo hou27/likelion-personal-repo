9c1ccbda735a3a16c109369365a762f348a641d2
hou27 <ataj125@gmail.com>
2023-04-27 20:33:22 +0900

    hw5: 5장 - #9
    module 분리

--- hw5/book-nest-backend/src/app.module.ts
+++ hw5/book-nest-backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [UsersController],
-  providers: [UsersService],
+  imports: [UsersModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
