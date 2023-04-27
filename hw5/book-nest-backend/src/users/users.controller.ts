4e89e4c9c6ae49f78f70f45b5352e6d5c668a5f5
hou27 <ataj125@gmail.com>
2023-04-27 20:07:09 +0900

    hw5: 4장 - #9
    mail 인증 로직 대신
    service 코드 완성

--- hw5/book-nest-backend/src/users/users.controller.ts
+++ hw5/book-nest-backend/src/users/users.controller.ts
@@ -3,29 +3,34 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UserLoginDto } from './dto/user-login.dto';
 import { VerifyEmailDto } from './dto/verify-email.dto';
 import { UserInfo } from './UserInfo';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
   @Post()
-  async createUser(@Body() dto: CreateUserDto): Promise<void> {
-    console.log(dto);
+  createUser(@Body() dto: CreateUserDto): void {
+    const { name, email, password } = dto;
+    this.usersService.createUser(name, email, password);
   }
 
-  @Post('/email-verify')
-  async verifyEmail(@Query() dto: VerifyEmailDto): Promise<string> {
-    console.log(dto);
-    return;
-  }
+  // @Post('/email-verify')
+  // async verifyEmail(@Query() dto: VerifyEmailDto): Promise<string> {
+  //   const { signupVerifyToken } = dto;
+
+  //   return await this.usersService.verifyEmail(signupVerifyToken);
+  // }
 
   @Post('/login')
-  async login(@Body() dto: UserLoginDto): Promise<string> {
-    console.log(dto);
-    return;
+  login(@Body() dto: UserLoginDto): string {
+    const { email, password } = dto;
+
+    return this.usersService.login(email, password);
   }
 
   @Get('/:id')
-  async getUserInfo(@Param('id') userId: string): Promise<UserInfo> {
-    console.log(userId);
-    return;
+  getUserInfo(@Param('id') userId: string): UserInfo {
+    return this.usersService.getUserInfo(userId);
   }
 }
