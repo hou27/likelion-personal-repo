8250245e9f52adc65c95ac4d0f5c129b9bcf4741
hou27 <ataj125@gmail.com>
2023-05-08 03:31:51 +0900

    hw7: posts domain 작업 - #14

--- hw7/instagram/src/posts/posts.controller.ts
+++ hw7/instagram/src/posts/posts.controller.ts
@@ -1,7 +1,36 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Headers,
+  Param,
+  ParseIntPipe,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { PostsService } from './posts.service';
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
+
+  @Post()
+  create(@Headers('userId') userId: string, @Body('content') content: string) {
+    return this.postsService.create(+userId, content);
+  }
+
+  @Get()
+  loadMyPosts(@Query('userId', ParseIntPipe) userId: number) {
+    return this.postsService.loadMyPosts(userId);
+  }
+
+  @Get(':id')
+  postDetail(@Param('id', ParseIntPipe) id: number) {
+    return this.postsService.postDetail(id);
+  }
+
+  @Get(':id/likes')
+  loadLikes(@Param('id', ParseIntPipe) id: number) {
+    return this.postsService.loadLikes(id);
+  }
 }
--- hw7/instagram/src/posts/posts.service.ts
+++ hw7/instagram/src/posts/posts.service.ts
@@ -1,4 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Post } from './entities/post.entity';
+import { UsersRepository } from 'src/users/users.repository';
+import { PostsRepository } from './posts.repository';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(
+    private readonly usersRepository: UsersRepository,
+    private readonly postsRepository: PostsRepository,
+  ) {}
+
+  create(userId: number, content: string): Post {
+    if (!userId) {
+      throw new UnauthorizedException('Login Needed');
+    }
+
+    const user = this.usersRepository.findOneById(userId);
+    if (!user) {
+      throw new NotFoundException('User Not Exist');
+    }
+
+    const newPost = this.postsRepository.save(
+      this.postsRepository.create(user.id, content),
+    );
+
+    return newPost;
+  }
+
+  loadMyPosts(userId: number): Post[] {
+    const user = this.usersRepository.findOneById(userId);
+    if (!user) {
+      throw new NotFoundException('User Not Exist');
+    }
+
+    return this.postsRepository.findByUserId(user.id);
+  }
+
+  postDetail(postId: number): Post {
+    const post = this.postsRepository.findOneById(postId);
+    if (!post) {
+      throw new NotFoundException('Post Not Exist');
+    }
+
+    return post;
+  }
+
+  loadLikes(postId: number): number {
+    const post = this.postsRepository.findOneById(postId);
+    if (!post) {
+      throw new NotFoundException('Post Not Exist');
+    }
+
+    return post.likes;
+  }
+}
