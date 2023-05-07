10162ec12fc3b3529c94bd798a76b4b06093904f
hou27 <ataj125@gmail.com>
2023-05-08 03:28:44 +0900

    hw7: 추가적인 프로젝트 세팅 - #14
    validation pipe 활성화
    NODE_ENV 설정 및 config module 설정

--- hw7/instagram/src/app.module.ts
+++ hw7/instagram/src/app.module.ts
@@ -11,6 +11,12 @@ import { validationSchema } from './config/validationSchema';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV === 'dev'
+          ? '.env.dev'
+          : process.env.NODE_ENV === 'prod'
+          ? '.env.prod'
+          : '.env.test',
       load: [emailConfig],
       validationSchema,
     }),
