ffe3bba410105e3b9114953077673d2da5067664
hou27 <ataj125@gmail.com>
2023-05-08 03:30:17 +0900

    hw7: users domain 작업 - #14

--- hw7/instagram/src/users/users.service.ts
+++ hw7/instagram/src/users/users.service.ts
@@ -1,26 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { UsersRepository } from './users.repository';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
+  constructor(private readonly usersRepository: UsersRepository) {}
 
-  findAll() {
-    return `This action returns all users`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  checkDuplicateUsername(username: string): boolean {
+    return this.usersRepository.checkDuplicateUsername(username);
   }
 }
